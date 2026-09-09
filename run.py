33ab6debf07b6e869f851ac55558ed1d6bf567d6
bikerbrandon2282 <bikerbrandon2282@gmail.com>
2026-09-09 19:14:09 +0100

    adding function to get last five entries of each item

--- run.py
+++ run.py
@@ -75,6 +75,20 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
 def main():
     """
     Run all program functions.
@@ -86,4 +100,5 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches Data Automation\n")
-main()
+# main()
+get_last_5_entries_sales()
